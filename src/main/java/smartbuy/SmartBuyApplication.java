1c6ca347e32d9c1dcb2f8edfb7a036f88f305823
Nicole <xixilele1990@gmail.com>
2026-01-20 19:56:05 -0800

    Inital commit

---
+++ src/main/java/smartbuy/SmartBuyApplication.java
@@ -0,0 +1,11 @@
+package smartbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBuyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBuyApplication.class, args);
+    }
+}
---
+++ src/test/java/smartbuy/SmartBuyApplicationTests.java
@@ -0,0 +1,12 @@
+package smartbuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SmartBuyApplication.class)
+class SmartBuyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
